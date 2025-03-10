d7c67c5cd84587d278f41d1eda658983cc0c2223
Igor Bessa <ibessa@ucsc.edu>
2025-03-10 16:50:53 -0700

    Pushing temp code

--- Assets/Resources/Ingredients/Flavors/FlavorIngredient.cs
+++ Assets/Resources/Ingredients/Flavors/FlavorIngredient.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Localization.Settings;
 
 [CreateAssetMenu(fileName = "New Item", menuName = "Ingredient/New Flavor Ingredient")]
 public class FlavorIngredient : Ingredient
@@ -84,14 +85,117 @@ public static readonly Dictionary<InflictionFlavor.InflictionType, Color> inflic
     { InflictionFlavor.InflictionType.UNAMI_Vampirism, new Color(0.58f, 0, 0.82f) } // Purple
 };
 
-public static readonly Dictionary<InflictionFlavor.InflictionType, string> inflictionTextMapping = new Dictionary<InflictionFlavor.InflictionType, string> {
-    { InflictionFlavor.InflictionType.SPICY_Burn, "Burn" },
-    { InflictionFlavor.InflictionType.FROSTY_Freeze, "Freeze" },
-    { InflictionFlavor.InflictionType.HEARTY_Health, "Health" },
-    { InflictionFlavor.InflictionType.SPIKY_Damage, "Damage" },
-    { InflictionFlavor.InflictionType.GREASY_Knockback, "Knockback" },
-    { InflictionFlavor.InflictionType.UNAMI_Vampirism, "Vampirism" }
+// --------------------- Dictionaries for localization below ------------------------
+public static readonly Dictionary<InflictionFlavor.InflictionType, string> EnglishFlavors = new Dictionary<InflictionFlavor.InflictionType, string>{
+    {InflictionFlavor.InflictionType.SPICY_Burn, "Burn"},
+    {InflictionFlavor.InflictionType.FROSTY_Freeze, "Freeze"},
+    {InflictionFlavor.InflictionType.HEARTY_Health, "Health"},
+    {InflictionFlavor.InflictionType.SPIKY_Damage, "Damage"},
+    {InflictionFlavor.InflictionType.GREASY_Knockback, "Knockback"},
+    {InflictionFlavor.InflictionType.UNAMI_Vampirism, "Vampirism"}
 };
+public static readonly Dictionary<InflictionFlavor.InflictionType, string> PortugueseFlavors = new Dictionary<InflictionFlavor.InflictionType, string>{
+    {InflictionFlavor.InflictionType.SPICY_Burn, "Queimadura"},
+    {InflictionFlavor.InflictionType.FROSTY_Freeze, "Congelar"},
+    {InflictionFlavor.InflictionType.HEARTY_Health, "Saúde"},
+    {InflictionFlavor.InflictionType.SPIKY_Damage, "Dano"},
+    {InflictionFlavor.InflictionType.GREASY_Knockback, "Recuo"},
+    {InflictionFlavor.InflictionType.UNAMI_Vampirism, "Vampirismo"}
+};
+public static readonly Dictionary<InflictionFlavor.InflictionType, string> SpanishFlavors = new Dictionary<InflictionFlavor.InflictionType, string>{
+    {InflictionFlavor.InflictionType.SPICY_Burn, "Quemadura"},
+    {InflictionFlavor.InflictionType.FROSTY_Freeze, "Congelar"},
+    {InflictionFlavor.InflictionType.HEARTY_Health, "Salud"},
+    {InflictionFlavor.InflictionType.SPIKY_Damage, "Daño"},
+    {InflictionFlavor.InflictionType.GREASY_Knockback, "Retroceso"},
+    {InflictionFlavor.InflictionType.UNAMI_Vampirism, "Vampirismo"}
+};
+
+public static Dictionary<InflictionFlavor.InflictionType, string> inflictionTextMapping = new Dictionary<InflictionFlavor.InflictionType, string>{
+    {InflictionFlavor.InflictionType.SPICY_Burn, "Burn"},
+    {InflictionFlavor.InflictionType.FROSTY_Freeze, "Freeze"},
+    {InflictionFlavor.InflictionType.HEARTY_Health, "Health"},
+    {InflictionFlavor.InflictionType.SPIKY_Damage, "Damage"},
+    {InflictionFlavor.InflictionType.GREASY_Knockback, "Knockback"},
+    {InflictionFlavor.InflictionType.UNAMI_Vampirism, "Vampirism"}
+};
+
+
+// 3
+
+// public static void InitializeInflictionTextMapping(UnityEngine.Localization.Locale locale)
+// {
+//     inflictionTextMapping.Clear();
+//     Dictionary<InflictionFlavor.InflictionType, string> selectedFlavors;
+//     switch (locale.LocaleName)
+//     {
+//         case "pt":
+//             selectedFlavors = new Dictionary<InflictionFlavor.InflictionType, string>(PortugueseFlavors);
+//             // inflictionTextMapping = PortugueseFlavors;
+//             break;
+//         case "es":
+//             selectedFlavors = SpanishFlavors;
+//             // inflictionTextMapping = SpanishFlavors;
+//             break;
+//         default:
+//             selectedFlavors = EnglishFlavors;
+//             // inflictionTextMapping = EnglishFlavors;
+//             break;
+//     }
+//     foreach (var flavor in selectedFlavors)
+//     {
+//         Debug.Log(flavor.Key + " " + flavor.Value);
+//         inflictionTextMapping.Add(flavor.Key, flavor.Value);
+//         Debug.Log(inflictionTextMapping);
+//     }
+// }
+
+// private void OnEnable()
+// {
+//     LocalizationSettings.SelectedLocaleChanged += OnLocaleChanged;
+//     if(LocalizationSettings.SelectedLocale != null)
+//     {
+//         inflictionTextMapping.Clear();
+//         Dictionary<InflictionFlavor.InflictionType, string> selectedFlavors;
+//         switch (LocalizationSettings.SelectedLocale.LocaleName)
+//         {
+//             case "pt":
+//                 selectedFlavors = PortugueseFlavors;
+//                 // inflictionTextMapping = PortugueseFlavors;
+//                 break;
+//             case "es":
+//                 selectedFlavors = SpanishFlavors;
+//                 // inflictionTextMapping = SpanishFlavors;
+//                 break;
+//             default:
+//                 selectedFlavors = EnglishFlavors;
+//                 // inflictionTextMapping = EnglishFlavors;
+//                 break;
+//         }
+//         foreach (var flavor in selectedFlavors)
+//         {
+//             Debug.Log(flavor.Key + " " + flavor.Value);
+//             inflictionTextMapping.Add(flavor.Key, flavor.Value);
+//         }
+//     }
+//     // InitializeInflictionTextMapping(LocalizationSettings.SelectedLocale);
+//     // switch (LocalizationSettings.SelectedLocale.LocaleName)
+//     // {
+//     //     case "en":
+//     //         inflictionTextMapping = EnglishFlavors;
+//     //         break;
+//     //     case "pt":
+//     //         inflictionTextMapping = PortugueseFlavors;
+//     //         break;
+//     //     case "es":
+//     //         inflictionTextMapping = SpanishFlavors;
+//     //         break;
+//     //     default:
+//     //         inflictionTextMapping = EnglishFlavors;
+//     //         break;
+//     // }
+
+// }
 
 }
 
